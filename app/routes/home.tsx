c592b9c2dc5a6e7d2d903e2e75f80f3f1ae59606
Khairul Alam <dev.khairul89@gmail.com>
2025-08-04 15:04:24 -0400

    Added navbar and mock data

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,3 +1,4 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
 
 export function meta({}: Route.MetaArgs) {
@@ -10,10 +11,19 @@ export function meta({}: Route.MetaArgs) {
 export default function Home() {
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
     <section className="main-section">
+      <Navbar />
+
       <div className="page-heading">
         <h1>Track Your Applications & Resume Strength</h1>
         <h2>Upgrade your resume by AI Powered Feedback</h2>
       </div>
     </section>
+
+    {[
+      {
+        title: 'Resume for X',
+        thumbnail: '..'
+      }
+    ]}
   </main>
 }
